bf9715f55c9fefba4085c0e3937eeebb1924cddb
Ken Vogel <ken@k2sw.com>
2019-10-27 22:23:31 -0400

    Updates

---
+++ precache-manifest.9f9909bc6e6a726f1a7e7ca38c7fe0ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740e72d3fe7a342df817c9e7c55f7f75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1601666e971501337b29",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cb37b911f8979fc1b83f",
+    "url": "/static/js/2.347702f4.chunk.js"
+  },
+  {
+    "revision": "1601666e971501337b29",
+    "url": "/static/js/main.af70b813.chunk.js"
+  },
+  {
+    "revision": "86fac645d051fb3d1c90",
+    "url": "/static/js/runtime-main.024091f7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
